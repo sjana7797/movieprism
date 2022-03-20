b15f9e7d0e96b223f32eca998014abc3ddc19e35
Sanjay Jana <sanjay.jana@valartech.com>
2022-03-20 13:45:11 +0530

    perf: adding image optimization

--- components/ui/Content.tsx
+++ components/ui/Content.tsx
@@ -18,6 +18,7 @@ function Content({ content }: { content: ContentOverview }) {
             className="rounded-md bg-black opacity-100 transition-opacity duration-200 group-hover:opacity-20"
             placeholder="blur"
             blurDataURL="https://dummyimage.com/208x288/fff/aaa"
+            sizes="208px"
           />
           <h2 className="absolute top-5 left-5 hidden group-hover:block">
             {content_name}
