204d0de72a9fff0c28b03287e8242506421991aa
Sanjay Jana <sanjay.jana@valartech.com>
2022-03-23 16:05:32 +0530

    refactor: removing meta fetcher

--- utils/getPostMetaData.ts
+++
@@ -1,8 +0,0 @@
-import fetchMetaData from "meta-fetcher";
-
-const getPostMetaData = async (url: string) => {
-  const search_description = await fetchMetaData(url);
-  return search_description.metadata.description;
-};
-
-export { getPostMetaData };
